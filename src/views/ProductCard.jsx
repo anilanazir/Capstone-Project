859161024fe02789b2bf8389d12a403213dd21f0
anilanazir <anilanazir7@gmail.com>
2025-12-16 17:47:06 +0500

    plant page updated

--- src/views/ProductCard.jsx
+++ src/views/ProductCard.jsx
@@ -3,18 +3,19 @@ import { ShoppingCart } from "lucide-react";
 import { useDispatch } from "react-redux";
 import { addToCart } from "@/features/cartSlice";
 import { cartUI } from "@/features/cartUi";
+import { Link } from "react-router-dom"; 
 
 const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
 
-   const handleAddToCart = () => {
+  const handleAddToCart = (e) => {
+    e.stopPropagation(); 
     dispatch(addToCart(product));
     cartUI.open(); 
   };
 
   return (
     <Card className="rounded-xl overflow-hidden shadow-md bg-[#f5f7f3] group relative">
-
       <button
         onClick={handleAddToCart}
         className="
@@ -26,26 +27,28 @@ const ProductCard = ({ product }) => {
         <ShoppingCart className="w-5 h-5 text-black" />
       </button>
 
-      <CardContent className="p-0 bg-[#f5f7f3]">
-        <div className="flex items-center justify-center h-80">
-          <img
-            src={product.image}
-            alt={product.name}
-            className="h-64 object-contain"
-          />
-        </div>
-      </CardContent>
+      <Link to={`/product/${product.id}`} className="block">
+        <CardContent className="p-0 bg-[#f5f7f3]">
+          <div className="flex items-center justify-center h-80">
+            <img
+              src={product.image}
+              alt={product.name}
+              className="h-64 object-contain"
+            />
+          </div>
+        </CardContent>
 
-      <CardHeader className="px-4 pt-2">
-        <h3 className="text-lg font-semibold">{product.name}</h3>
-        <p className="text-gray-500 text-sm">{product.description}</p>
-      </CardHeader>
+        <CardHeader className="px-4 pt-2">
+          <h3 className="text-lg font-semibold">{product.name}</h3>
+          <p className="text-gray-500 text-sm">{product.description}</p>
+        </CardHeader>
 
-      <CardFooter className="px-4 pb-4">
-        <p className="text-green-700 font-bold text-md">
-          ${product.price.toFixed(2)}
-        </p>
-      </CardFooter>
+        <CardFooter className="px-4 pb-4">
+          <p className="text-green-700 font-bold text-md">
+            ${product.price.toFixed(2)}
+          </p>
+        </CardFooter>
+      </Link>
     </Card>
   );
 };
